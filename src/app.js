aecd80cf518c0bd777250a50f58c8c4be776d2f6
JortvD <jortvandriel@hotmail.com>
2018-10-14 21:12:04 +0200

    Added the created classes to the application

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
+const GlobalConfigFile = require("./config/global");
+const PanelClient = require("./net/panel/client");
+
 module.exports = class Application {
 	constructor() {
-
+		this.globalConfig = new GlobalConfigFile();
+		this.panelClient = new PanelClient();
 	}
 
 	async start() {
-
+		await this.globalConfig.load();
+		await this.panelClient.connect();
 	}
 
 	async stop() {
-		
+		await this.panelClient.disconnect();
 	}
 }
