8749469cb7633de01b0f0d6f96b801c59166f53c
JortvD <jortvandriel@hotmail.com>
2018-10-19 17:27:46 +0200

    Add default state for PanelClient

--- src/net/panel/client.js
+++ src/net/panel/client.js
@@ -4,6 +4,7 @@ const Encryption = require("./../encryption");
 module.exports = class PanelClient {
 	constructor() {
 		this.encryption = new Encryption();
+		this.state = "authenticating";
 	}
 
 	connect() {
