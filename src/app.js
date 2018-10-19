daf19dc616664982593426afa622588292ede586
JortvD <jortvandriel@hotmail.com>
2018-10-19 17:27:05 +0200

    Add more config settings

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 const GlobalConfigFile = require("./config/global");
+const PrivateConfigFile = require("./config/private");
 const PanelClient = require("./net/panel/client");
 
 module.exports = class Application {
 	constructor() {
 		this.globalConfig = new GlobalConfigFile();
+		this.privateConfig = new PrivateConfigFile();
 		this.panelClient = new PanelClient();
 	}
 
 	async start() {
 		await this.globalConfig.load();
+		await this.privateConfig.load();
 		await this.panelClient.connect();
 	}
 
