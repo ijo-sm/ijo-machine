3f3291d20886a186a9d7646f24c0288309feb67f
JortvD <jortvandriel@hotmail.com>
2018-11-04 11:33:05 +0100

    Implement everything in the app class

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ const GlobalConfigFile = require("./config/global");
 const PrivateConfigFile = require("./config/private");
 const PanelClient = require("./net/panel/client");
 const Database = require("@ijo-sm/helper-database");
-const NodeListManager = require("./node/manager");
+const NodeManager = require("./node/manager");
+const PluginManager = require("./plugin/manager");
 const UtilsManager = require("@ijo-sm/utils");
 
 module.exports = class Application {
@@ -15,19 +16,23 @@ module.exports = class Application {
 		this.globalConfig = new GlobalConfigFile();
 		this.privateConfig = new PrivateConfigFile();
 		this.panelClient = new PanelClient();
-		this.db = new Database(this.utils.path.resolve("../data/panel.json"));
-		this.nodeLists = new NodeListManager();
+		this.plugins = new PluginManager();
+		this.db = new Database(this.utils.path.resolve("../data/machine.json"));
+		this.nodes = new NodeManager();
 	}
 
 	async start() {
+		await this.nodes.initialize();
 		await this.db.load();
-		await this.nodeLists.initialize();
+		await this.plugins.load();
+		await this.nodes.load();
 		await this.globalConfig.load();
 		await this.privateConfig.load();
 		await this.panelClient.connect();
 	}
 
 	async stop() {
+		await this.nodes.save();
 		await this.globalConfig.save();
 		await this.privateConfig.save();
 		await this.panelClient.disconnect();
