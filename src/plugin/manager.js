1d282a20912811dc1ab4259506d60b330f73dbad
JortvD <jortvandriel@hotmail.com>
2018-11-04 11:32:41 +0100

    Add the beginnings of the plugin manager

---
+++ src/plugin/manager.js
@@ -0,0 +1,30 @@
+const NodeUtils = require("util");
+const FileSystem = require("fs");
+
+module.exports = class PluginManager {
+	constructor() {
+		this.plugins = new Map();
+	}
+
+	async load() {
+		let paths = await NodeUtils.promisify(FileSystem.readdir)(ijo.utils.path.resolve("plugins/"));
+
+		paths.forEach(function(path) {
+			let plugin;
+
+			/*try {
+				plugin = loadPlugin(path);
+			} catch(e) {
+				return console.error(`The plugin at /panel/plugins/${path} could not be loaded: ${e.message}`);
+			}
+			
+			if(this.plugins.has(plugin.name)) {
+				return console.error(`The plugin ${plugin.name} has already been loaded`);
+			}
+
+			plugin.initialize();
+
+			this.plugins.set(plugin.name, plugin);*/
+		}.bind(this));
+	}
+}
